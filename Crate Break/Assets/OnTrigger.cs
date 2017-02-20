3b2079e7db5a525b6a9182307ce27fd9e139a412
Unknown <justinleroyklosinski@gmail.com>
2017-02-19 21:11:27 -0800

    explosion scripts need finishing

---
+++ Crate Break/Assets/OnTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class OnTrigger : MonoBehaviour {
+
+    public static Action<Transform> explodehere;
+    
+	// Use this for initialization
+	void OnTriggerEnter() {
+        explodehere(this);
+    }
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
